3eeedea80f6287b8bf5add11bb4c9924d492b7b8
vcarvalho0 <vitor1.santos00@gmail.com>
2023-02-19 01:18:23 -0300

    feat: add jest

---
+++ jest.config.ts
@@ -0,0 +1,23 @@
+import type { Config } from 'jest'
+import { pathsToModuleNameMapper } from 'ts-jest'
+import { compilerOptions } from './tsconfig.json'
+
+const config: Config = {
+  preset: 'ts-jest',
+  testEnvironment: 'node',
+  clearMocks: true,
+  globals: {
+    'ts-jest': {
+      isolatedModules: true
+    }
+  },
+  modulePathIgnorePatterns: ['<rootDir/dist>'],
+  moduleNameMapper: pathsToModuleNameMapper(
+    compilerOptions.paths,
+    {
+      prefix: '<rootDir>'
+    }
+  )
+}
+
+export default config
