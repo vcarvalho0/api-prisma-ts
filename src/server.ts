6fa2f03dcdf517828ac0f569dbd6d947c50deded
vcarvalho0 <vitor1.santos00@gmail.com>
2023-02-21 23:12:59 -0300

    feat: add cors

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,12 @@
 import express from 'express'
+import cors from 'cors'
 const PORT = 3000
 
 const app = express()
 
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
+app.use(cors)
 
 app.listen(PORT, () => {
   console.log(`🚀 Server is running at ${PORT}`)
